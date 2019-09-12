59bacf59a71a1db0fb4eb7199fd61dd07f674759
kyle <kylechen49@gmail.com>
2019-09-12 12:43:37 -0400

    [CODE] Able to init player

--- src/main/java/me/largetimmo/comp4004/a1/service/ServerGameManager.java
+++ src/main/java/me/largetimmo/comp4004/a1/service/ServerGameManager.java
@@ -3,11 +3,15 @@ package me.largetimmo.comp4004.a1.service;
 
 import lombok.extern.slf4j.Slf4j;
 import me.largetimmo.comp4004.a1.controller.ServerMessageIOController;
+import me.largetimmo.comp4004.a1.service.bo.LowerSectionGameScore;
 import me.largetimmo.comp4004.a1.service.bo.PlayerBO;
+import me.largetimmo.comp4004.a1.service.bo.ScoreSheetBO;
+import me.largetimmo.comp4004.a1.service.bo.UpperSectionGameScore;
 
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 public class ServerGameManager {
@@ -27,8 +31,12 @@ public class ServerGameManager {
             throw new RuntimeException("Failed to init player.");
         }
         PlayerBO playerBO = new PlayerBO();
-
-
+        playerBO.setPlayerId(UUID.randomUUID().toString());
+        playerBO.setClient(connection);
+        ScoreSheetBO scoreSheet = new ScoreSheetBO();
+        scoreSheet.setLowerSection(new LowerSectionGameScore());
+        scoreSheet.setUpperSection(new UpperSectionGameScore());
+        playerBO.setScoreSheet(scoreSheet);
 
         return playerBO;
     }
--- src/main/java/me/largetimmo/comp4004/a1/service/bo/ScoreSheetBO.java
+++ src/main/java/me/largetimmo/comp4004/a1/service/bo/ScoreSheetBO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class ScoreSheetBO {
 
-    private ScoreSectionBO upperSection;
+    private UpperSectionGameScore upperSection;
 
-    private ScoreSectionBO lowerSection;
+    private LowerSectionGameScore lowerSection;
 }
