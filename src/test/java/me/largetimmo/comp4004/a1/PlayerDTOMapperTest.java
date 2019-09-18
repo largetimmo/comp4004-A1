d9c8a667bfd958b839cea256acca923d6a3d1ccb
kyle <kylechen49@gmail.com>
2019-09-18 15:24:04 -0400

    [TEST] Add player dto mapper test

---
+++ src/test/java/me/largetimmo/comp4004/a1/PlayerDTOMapperTest.java
@@ -0,0 +1,39 @@
+package me.largetimmo.comp4004.a1;
+
+import me.largetimmo.comp4004.a1.configuration.dto.PlayerDTO;
+import me.largetimmo.comp4004.a1.configuration.dto.mapper.PlayerDTOMapper;
+import me.largetimmo.comp4004.a1.service.bo.Connection;
+import me.largetimmo.comp4004.a1.service.bo.PlayerBO;
+import me.largetimmo.comp4004.a1.service.bo.ScoreSheetBO;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PlayerDTOMapperTest {
+
+    private PlayerDTOMapper mapper = new PlayerDTOMapper();
+
+    @Test
+    public void testBoToDto() throws Exception{
+        PlayerBO playerBO = new PlayerBO();
+        playerBO.setPlayerId("111");
+        playerBO.setPlayerName("name");
+        playerBO.setScoreSheet(new ScoreSheetBO());
+        PlayerDTO dto = mapper.map(playerBO);
+        Assert.assertEquals(dto.getPlayerId(), playerBO.getPlayerId());
+        Assert.assertEquals(dto.getPlayerName(), playerBO.getPlayerName());
+        Assert.assertEquals(dto.getScoreSheet(), playerBO.getScoreSheet());
+    }
+    @Test
+    public void testDtoToBo() throws Exception{
+        PlayerDTO playerDTO = new PlayerDTO();
+        playerDTO.setPlayerId("111");
+        playerDTO.setPlayerName("name");
+        playerDTO.setScoreSheet(new ScoreSheetBO());
+        PlayerBO bo = mapper.map(playerDTO);
+        Assert.assertEquals(bo.getPlayerId(), playerDTO.getPlayerId());
+        Assert.assertEquals(bo.getPlayerName(), playerDTO.getPlayerName());
+        Assert.assertEquals(bo.getScoreSheet(), playerDTO.getScoreSheet());
+        Assert.assertNull(bo.getConnection());
+    }
+
+}
