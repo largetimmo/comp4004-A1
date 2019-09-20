6f6171ac2a52d80d2c8484b59b0b66d32fe3617b
kyle <kylechen49@gmail.com>
2019-09-20 18:30:46 -0400

    [CODE] Add lower section calculate total

--- src/main/java/me/largetimmo/comp4004/a1/service/bo/LowerSectionGameScore.java
+++ src/main/java/me/largetimmo/comp4004/a1/service/bo/LowerSectionGameScore.java
@@ -6,24 +6,26 @@ import lombok.Data;
 @Data
 public class LowerSectionGameScore {
 
-    private Integer threeOfAKind;
+    private Integer threeOfAKind = 0;
 
-    private Integer fourOfAKind;
+    private Integer fourOfAKind = 0;
 
-    private Integer fullHouse;
+    private Integer fullHouse = 0;
 
-    private Integer smallStraight;
+    private Integer smallStraight = 0;
 
-    private Integer largeStraight;
+    private Integer largeStraight = 0;
 
-    private Integer Yahtzee;
+    private Integer Yahtzee = 0;
 
-    private Integer chance;
-
-    private Integer boundCount = 0;
+    private Integer chance = 0;
 
     private Integer YahtzeeBonus = 0;
 
-    private Integer total;
+    private Integer total = 0;
+
+    public void  calculateTotal(){
+        total = threeOfAKind + fourOfAKind + fullHouse + smallStraight + largeStraight + Yahtzee + chance + YahtzeeBonus*100;
+    }
 
 }
