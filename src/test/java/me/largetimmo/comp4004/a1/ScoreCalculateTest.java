1423fc9e2e77d8a009a1a1c18abb2d354a689fb5
kyle <kylechen49@gmail.com>
2019-09-20 18:26:26 -0400

    [TEST] Add lower section total test

--- src/test/java/me/largetimmo/comp4004/a1/ScoreCalculateTest.java
+++ src/test/java/me/largetimmo/comp4004/a1/ScoreCalculateTest.java
@@ -1,5 +1,6 @@
 package me.largetimmo.comp4004.a1;
 
+import me.largetimmo.comp4004.a1.service.bo.LowerSectionGameScore;
 import me.largetimmo.comp4004.a1.service.bo.UpperSectionGameScore;
 import org.junit.Assert;
 import org.junit.Test;
@@ -22,6 +23,27 @@ public class ScoreCalculateTest {
         upperSectionGameScore.setFours(8);
         upperSectionGameScore.calculateTotal();
         Assert.assertEquals(106, (int) upperSectionGameScore.getTotal());
+
+    }
+
+    @Test
+    public void calculateLowerSectionBonus(){
+        LowerSectionGameScore lowerSectionGameScore  = new LowerSectionGameScore();
+        Assert.assertEquals(0, (int)lowerSectionGameScore.getTotal());
+        lowerSectionGameScore.setYahtzee(50);
+        lowerSectionGameScore.calculateTotal();
+        Assert.assertEquals(50,(int)lowerSectionGameScore.getTotal());
+        lowerSectionGameScore.setYahtzeeBonus(2);
+        lowerSectionGameScore.calculateTotal();
+        Assert.assertEquals(250,(int)lowerSectionGameScore.getTotal());
+        lowerSectionGameScore.setFourOfAKind(40);
+        lowerSectionGameScore.setThreeOfAKind(30);
+        lowerSectionGameScore.setFullHouse(25);
+        lowerSectionGameScore.setSmallStraight(30);
+        lowerSectionGameScore.calculateTotal(); 
+        Assert.assertEquals(375,(int)lowerSectionGameScore.getTotal());
+
+
 
     }
 
