51687c02ea2f5dab7fd785381450e74a9382d2a3
kyle <kylechen49@gmail.com>
2019-09-21 13:18:32 -0400

    [TEST] Add test to score calculation

--- src/test/java/me/largetimmo/comp4004/a1/ScoreCalculateTest.java
+++ src/test/java/me/largetimmo/comp4004/a1/ScoreCalculateTest.java
@@ -1,11 +1,9 @@
 package me.largetimmo.comp4004.a1;
 
 import me.largetimmo.comp4004.a1.service.ServerGameManager;
-import me.largetimmo.comp4004.a1.service.bo.LowerSectionGameScore;
-import me.largetimmo.comp4004.a1.service.bo.PlayerBO;
-import me.largetimmo.comp4004.a1.service.bo.ScoreSheetBO;
-import me.largetimmo.comp4004.a1.service.bo.UpperSectionGameScore;
+import me.largetimmo.comp4004.a1.service.bo.*;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -15,6 +13,17 @@ import java.util.List;
 public class ScoreCalculateTest {
 
     private ServerGameManager serverGameManager = new ServerGameManager(null,null);
+
+    private PlayerBO playerBO;
+
+    private ScoreSheetBO scoreSheetBO;
+
+    @Before
+    public void setup(){
+        playerBO = new PlayerBO();
+        scoreSheetBO = new ScoreSheetBO();
+        playerBO.setScoreSheet(scoreSheetBO);
+    }
 
     @Test
     public void calculateUpperSectionBonus(){
@@ -89,6 +98,146 @@ public class ScoreCalculateTest {
         scoreSheetBO.calculateTotal();
         Assert.assertEquals(106+375,(int)scoreSheetBO.getTotal());
 
+    }
+
+    @Test
+    public void testONES_1(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(1,1,1,1,1), ScoreCategory.ONES);
+        Assert.assertEquals(5,(int)scoreSheetBO.getTotal());
+    }
+    @Test
+    public void testONES_2(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(1,1,1,2,2), ScoreCategory.ONES);
+        Assert.assertEquals(3,(int)scoreSheetBO.getTotal());
+    }
+    @Test
+    public void testONES_3(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(1,2,3,4,5), ScoreCategory.ONES);
+        Assert.assertEquals(1,(int)scoreSheetBO.getTotal());
+    }
+
+    @Test
+    public void testTWO_1(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(1,2,3,4,5), ScoreCategory.TWOS);
+        Assert.assertEquals(2,(int)scoreSheetBO.getTotal());
+    }
+    @Test
+    public void testTWO_2(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,2,2,2,2), ScoreCategory.TWOS);
+        Assert.assertEquals(10,(int)scoreSheetBO.getTotal());
+    }
+    @Test
+    public void testTWO_3(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,3,3,3,3), ScoreCategory.TWOS);
+        Assert.assertEquals(2,(int)scoreSheetBO.getTotal());
+    }
+
+    @Test
+    public void testTHREE(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,3,3,3,3), ScoreCategory.THREES);
+        Assert.assertEquals(12,(int)scoreSheetBO.getTotal());
+    }
+
+    @Test
+    public void testFOURS(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,3,3,4,3), ScoreCategory.FOURS);
+        Assert.assertEquals(4,(int)scoreSheetBO.getTotal());
+    }
+
+    @Test
+    public void testFIVE(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,3,4,5,5), ScoreCategory.FIVES);
+        Assert.assertEquals(10,(int)scoreSheetBO.getTotal());
+    }
+
+    @Test
+    public void testSIX(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,3,6,6,6), ScoreCategory.SIXES);
+        Assert.assertEquals(18,(int)scoreSheetBO.getTotal());
+    }
+
+    @Test
+    public void testThreeOfKind(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,3,6,6,6), ScoreCategory.THREE_OF_A_KIND);
+        Assert.assertEquals(2+3+6+6+6,(int)scoreSheetBO.getTotal());
+    }
+
+    @Test
+    public void testFourOFKind(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,6,6,6,6), ScoreCategory.FOUR_OF_A_KIND);
+        Assert.assertEquals(2+6+6+6+6,(int)scoreSheetBO.getTotal());
+    }
+    @Test
+    public void testFullHouse_1(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,2,6,6,6), ScoreCategory.FULL_HOUSE);
+        Assert.assertEquals(25,(int)scoreSheetBO.getTotal());
+    }
+    @Test
+    public void testFullHouse_2(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,3,6,6,6), ScoreCategory.FULL_HOUSE);
+        Assert.assertEquals(0,(int)scoreSheetBO.getTotal());
+    }
+    @Test
+    public void testSmallStraight_1(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(1,2,3,5,6), ScoreCategory.SMALL_STRAIGHT);
+        Assert.assertEquals(0,(int)scoreSheetBO.getTotal());
+    }
+    @Test
+    public void testSmallStraight_2(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(1,2,3,4,6), ScoreCategory.SMALL_STRAIGHT);
+        Assert.assertEquals(30,(int)scoreSheetBO.getTotal());
+    }
+    @Test
+    public void testLargeStraight_1(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(1,2,3,4,6), ScoreCategory.LARGE_STRAIGHT);
+        Assert.assertEquals(0,(int)scoreSheetBO.getTotal());
+    }
+    @Test
+    public void testLargeStraight_2(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(1,2,3,4,5), ScoreCategory.LARGE_STRAIGHT);
+        Assert.assertEquals(40,(int)scoreSheetBO.getTotal());
+    }
+
+    @Test
+    public void testYAHTZEE_1(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(1,2,3,4,5), ScoreCategory.YAHTZEE);
+        Assert.assertEquals(0,(int)scoreSheetBO.getTotal());
+    }
+    @Test
+    public void testYAHTZEE_2(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,2,2,2,2), ScoreCategory.YAHTZEE);
+        Assert.assertEquals(50,(int)scoreSheetBO.getTotal());
+    }
+    @Test
+    public void testYAHTZEE_3(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,2,2,2,2), ScoreCategory.YAHTZEE);
+        Assert.assertEquals(50,(int)scoreSheetBO.getTotal());
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,2,2,2,2), ScoreCategory.YAHTZEE);
+        Assert.assertEquals(150,(int)scoreSheetBO.getTotal());
+    }
+    @Test
+    public void testYAHTZEE_4(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,2,2,2,2), ScoreCategory.YAHTZEE);
+        Assert.assertEquals(50,(int)scoreSheetBO.getTotal());
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,2,2,2,2), ScoreCategory.YAHTZEE);
+        Assert.assertEquals(150,(int)scoreSheetBO.getTotal());
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,2,2,2,2), ScoreCategory.YAHTZEE);
+        Assert.assertEquals(250,(int)scoreSheetBO.getTotal());
+    }
+
+    @Test
+    public void testChance(){
+        serverGameManager.calculateScoreForPlayer(playerBO,createDiceArray(2,4,5,3,2), ScoreCategory.CHANCE);
+        Assert.assertEquals(2+4+5+3+2,(int)scoreSheetBO.getTotal());
+    }
+
+
+
+    private List<Integer> createDiceArray(Integer... dices){
+        if (dices.length != 5){
+            System.out.println("Incorrect dice quantity. Check your code.");
+        }
+        return new ArrayList<>(Arrays.asList(dices));
     }
 
 }
