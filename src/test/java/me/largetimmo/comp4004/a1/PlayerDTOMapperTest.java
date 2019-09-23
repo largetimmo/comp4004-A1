11a56213032631a56feee4c53052ae76a388d732
kyle <kylechen49@gmail.com>
2019-09-23 15:47:04 -0400

    [TEST] DTO mapper should include rounds

--- src/test/java/me/largetimmo/comp4004/a1/PlayerDTOMapperTest.java
+++ src/test/java/me/largetimmo/comp4004/a1/PlayerDTOMapperTest.java
@@ -18,10 +18,13 @@ public class PlayerDTOMapperTest {
         playerBO.setPlayerId("111");
         playerBO.setPlayerName("name");
         playerBO.setScoreSheet(new ScoreSheetBO());
+        playerBO.setRound(11);
         PlayerDTO dto = mapper.map(playerBO);
         Assert.assertEquals(dto.getPlayerId(), playerBO.getPlayerId());
         Assert.assertEquals(dto.getPlayerName(), playerBO.getPlayerName());
         Assert.assertEquals(dto.getScoreSheet(), playerBO.getScoreSheet());
+        Assert.assertEquals(dto.getRound(),playerBO.getRound());
+
     }
     @Test
     public void testDtoToBo() throws Exception{
@@ -29,11 +32,14 @@ public class PlayerDTOMapperTest {
         playerDTO.setPlayerId("111");
         playerDTO.setPlayerName("name");
         playerDTO.setScoreSheet(new ScoreSheetBO());
+        playerDTO.setRound(12);
         PlayerBO bo = mapper.map(playerDTO);
         Assert.assertEquals(bo.getPlayerId(), playerDTO.getPlayerId());
         Assert.assertEquals(bo.getPlayerName(), playerDTO.getPlayerName());
         Assert.assertEquals(bo.getScoreSheet(), playerDTO.getScoreSheet());
         Assert.assertNull(bo.getConnection());
+        Assert.assertEquals(playerDTO.getRound(),bo.getRound());
+
     }
 
 }
