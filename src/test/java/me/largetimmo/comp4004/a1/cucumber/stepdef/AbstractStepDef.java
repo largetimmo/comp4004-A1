85e54eaea7c5f3fe6b8fd1dc49e52e2e50ea48e0
kyle <kylechen49@gmail.com>
2019-10-10 10:52:40 -0400

    [TEST] Intergration with Spring boot

---
+++ src/test/java/me/largetimmo/comp4004/a1/cucumber/stepdef/AbstractStepDef.java
@@ -0,0 +1,15 @@
+package me.largetimmo.comp4004.a1.cucumber.stepdef;
+
+import me.largetimmo.comp4004.a1.AutoTestContext;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {AutoTestContext.class})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@SpringBootTest
+public abstract class AbstractStepDef {
+}
